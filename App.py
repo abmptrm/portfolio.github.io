c4490731df72affb7345b6455d3b918713672f09
xlnx <50400974+ariobimox@users.noreply.github.com>
2021-12-07 09:43:23 +0700

    Update App.py

--- App.py
+++ App.py
@@ -3,15 +3,6 @@ from flask import render_template
 
 app = Flask(__name__)
 
-
-# @app.route("/")
-# def main():
-#     return render_template('index.html')
-
-# @app.route("/s/<username>")
-# def main(username):
-#     return f"{render_template('index.html')} <script>alert('Halo {username}')</script>"
-
 @app.route("/home")
 def about():
     return render_template('index.html')
@@ -19,6 +10,10 @@ def about():
 @app.route("/project")
 def project():
     return render_template("project.html")
+
+@app.route("/cv")
+def cv():
+    return render_template("cv.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
